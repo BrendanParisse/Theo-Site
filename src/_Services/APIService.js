b20fbacb52d0dbc186e50ac57cf2173e0de87879
BrendanP <brendan.parisse@yahoo.fr>
2024-02-26 19:02:37 +0100

    V8

--- src/_Services/APIService.js
+++ src/_Services/APIService.js
@@ -1,4 +1,4 @@
-const token = process.env.REACT_APP_API_TOKEN;
+const token = 'e98e1b6a60e1cce2296a55b9bbb7a62e16436ddea249e42b733df5df240520867e8a9fa4ee90332b8cc7d1d38b40ef8492a07a60c93e912e2ce184470272893f5e6b65fa3fb6a2c0668e4daedd88dd5fda997e557622a86f784a70fe60d071c9e1d55afd4e6c63b3701142bb694a7bc25d03f6aadcadb1a8edc87019c1c9d5e0';
 
 export async function ApiDataAccueil() {
     try {
